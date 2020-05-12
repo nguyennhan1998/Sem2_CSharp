4129ad0773a5ba95cb704d871f87ad4ed017771b
Nguyen nhan <nguyennnhanlovemyself@gmail.com>
2020-05-12 09:57:40 +0700

    make it better

---
+++ Properties/Practical/Cylinder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AP.Properties.Practical
+{
+    public class Cylinder
+    {
+        private double baseArea;
+        private double lareralArca;
+        private double totalArea;
+        private double volume;
+
+        public void Caculate(double radius, double height)
+        {
+            baseArea =  radius * radius * Math.PI;
+            lareralArca = 2 * Math.PI * radius * height;
+            totalArea = 2 * Math.PI * radius * (height + radius);
+            volume = Math.PI * radius * radius * height;
+        }
+
+        public void Result()
+        {
+            Console.WriteLine("base: "+this.baseArea+
+                              "\nlateral: "+this.lareralArca+
+                              "\nTotal: "+this.totalArea+
+                              "\nVolume: "+this.volume);
+        }
+    }
+}
---
+++ Properties/Practical/Geometry.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AP.Properties.Practical
+{
+    public class Geometry
+    {
+        public static void Main(string[] args)
+        {
+            Cylinder c1=new Cylinder();
+            Console.WriteLine("Enter the dimension of the cynlinder: ");
+            Console.WriteLine("Radius: ");
+            double radius = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Height: ");
+            double height = Convert.ToDouble(Console.ReadLine());
+            c1.Caculate(radius,height);
+            Console.WriteLine("Cylinder characteristics");
+            Console.WriteLine("Radius: "+radius);
+            Console.WriteLine("height: "+height);
+            c1.Result();
+        }
+    }
+}
